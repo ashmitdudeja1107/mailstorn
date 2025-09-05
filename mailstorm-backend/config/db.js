77026243c1c3bcd33400c74d96523756597714fb
ashmitdudeja1107 <166710701+ashmitdudeja1107@users.noreply.github.com>
2025-09-06 03:00:11 +0530

    Update db.js

--- mailstorm-backend/config/db.js
+++ mailstorm-backend/config/db.js
@@ -6,7 +6,7 @@ console.log("DATABASE_URL:", process.env.DATABASE_URL ? "***" : "NOT SET");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false }, 
 });
 
 module.exports = pool;
